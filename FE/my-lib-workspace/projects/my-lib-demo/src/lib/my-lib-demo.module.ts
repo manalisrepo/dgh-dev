b166400897704462c6a7f5e72b97806f240759ff
knottyalok <alokadbg@gmail.com>
2025-02-11 18:27:13 +0530

    Added changes for lib

---
+++ FE/my-lib-workspace/projects/my-lib-demo/src/lib/my-lib-demo.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MyLibDemoComponent } from './my-lib-demo.component';
+
+@NgModule({
+  declarations: [MyLibDemoComponent],
+  imports: [],
+  exports: [MyLibDemoComponent],
+})
+export class MyLibDemoModule {}
---
+++ FE/my-lib-workspace/projects/my-lib-demo/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of my-lib-demo
+ */
+
+export * from './lib/my-lib-demo.service';
+// export * from './lib/my-lib-demo.component';
+export * from './lib/my-lib-demo.module';
