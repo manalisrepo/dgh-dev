0605da270c4c223dd6b30f2b67e40f1674760bd7
kancharlasreya <kancharlasreya3@gmail.com>
2025-02-12 11:21:11 +0530

    changes in ui-login

--- FE/ui-login/src/app/app-routing.module.ts
+++ FE/ui-login/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { LoginComponent } from '../components/login/login.component';
 
-const routes: Routes = [
-  {path:"", component: EmptyRouteComponent}
-];
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers:[{
-    provide: APP_BASE_HREF, useValue: '/login'
-  }]
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/login',
+    },
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
