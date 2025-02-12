cc753b7fa82499a3d0d2a7b8a8f69568db71de9d
Jinal Gandhi <gandhijinal07@gmail.com>
2025-02-12 14:39:54 +0530

    setup library project

--- FE/my-lib-workspace/projects/my-lib-demo/src/public-api.ts
+++ FE/my-lib-workspace/projects/my-lib-demo/src/public-api.ts
@@ -3,5 +3,5 @@
  */
 
 export * from './lib/my-lib-demo.service';
-// export * from './lib/my-lib-demo.component';
+export * from './lib/my-lib-demo.component';
 export * from './lib/my-lib-demo.module';
