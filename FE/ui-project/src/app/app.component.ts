a216a9f2a00e2249aad5d7f49cd72b07fa521cd2
Jinal Gandhi <gandhijinal07@gmail.com>
2025-02-28 15:37:11 +0530

    loader, alert, modal library

--- FE/ui-project/src/app/app.component.ts
+++ FE/ui-project/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { ModalService } from './modal.service';
+import { AlertLibService } from 'alert-lib';
+
+import { LoaderLibService } from 'loader-lib';
+import { ModalLibService } from 'modal-lib';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'ui-project';
-  constructor() {}
+  modalConfig: any;
+  id = '';
+
+  constructor(
+    private modalService: ModalService,
+    private modalLibService: ModalLibService,
+    private alertService: AlertLibService,
+    private loaderService: LoaderLibService
+  ) {}
   ngOnInit(): void {}
+
+  //  call this function to open modal by passing modal id
+  openModal(id: string) {
+    this.loaderService.showLoader(true, 'Saving....');
+    this.modalConfig = {
+      modalTitle: 'alert',
+      dismissButtonLabel: 'Cancel',
+      closeButtonLabel: 'OK',
+      disableCloseButton: false,
+      disableDismissButton: false,
+      hideCloseButton: false,
+      hideDismissButton: false,
+    };
+    this.id = id;
+    this.modalLibService.open(id);
+  }
+
+  // just call this function to close modal by passing modal id
+  closeModal(id: string) {
+    this.modalConfig = '';
+    this.id = '';
+    this.modalLibService.close(id);
+  }
+
+  alertOpen() {
+    this.loaderService.hideLoader();
+    this.alertService.info(
+      'For first time data update for migrated users, once you save the field level information on the form, it cannot be edited further. If you need to make further changes, you can do so through separate project update service subsequently.'
+    );
+  }
+
+  show(event: any) {
+    this.modalLibService.close(this.id);
+    this.modalConfig = '';
+    this.id = '';
+    console.log(event);
+  }
 }
--- FE/ui-project/src/app/app.module.ts
+++ FE/ui-project/src/app/app.module.ts
@@ -9,10 +9,29 @@ import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 import { MyLibDemoModule } from 'my-lib-demo';
+import { AlertLibModule } from 'alert-lib';
+import { LoaderLibModule } from 'loader-lib';
+import { ModalLibModule } from 'modal-lib';
+import { ModalComponent } from './modal/modal.component';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent, EmptyRouteComponent],
-  imports: [BrowserModule, AppRoutingModule, MyLibDemoModule],
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent,
+    ModalComponent,
+    LoaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    MyLibDemoModule,
+    AlertLibModule,
+    ModalLibModule,
+    LoaderLibModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
