4309a1e9abc7d691759dc0a3dbc588c47f175fe0
Jinal Gandhi <gandhijinal07@gmail.com>
2025-03-07 11:45:46 +0530

    Header created

---
+++ FE/ui-project/src/app/components/firstcomponent/firstcomponent.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firstcomponent',
+  templateUrl: './firstcomponent.component.html',
+  styleUrl: './firstcomponent.component.scss',
+})
+export class FirstcomponentComponent {}
