6e00fa9bc6be36e0da9241bec77e3969217528d6
Arya Das <arya.das512ht@gmail.com>
2025-03-11 13:17:38 +0530

    login screen css update

--- FE/ui-login/src/app/components/login/login.component.ts
+++ FE/ui-login/src/app/components/login/login.component.ts
@@ -8,13 +8,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./login.component.scss'],
   animations: [
     trigger('slideInOut', [
-      state('hidden', style({ transform: 'translateX(100%)', opacity: 0 })),
-      state('visible', style({ transform: 'translateX(0)', opacity: 1 })),
+      state('hidden', style({ transform: 'translateX(100%)', opacity: 0 })),  
+      state('visible', style({ transform: 'translateX(0)', opacity: 1 })),  
   
-      transition('hidden => visible', animate('0.6s cubic-bezier(0.25, 1, 0.5, 1)')),
-      transition('visible => hidden', animate('0.5s cubic-bezier(0.5, 0, 0.75, 0)'))
+      transition('hidden => visible', animate('0.9s cubic-bezier(0.16, 1, 0.3, 1)')),  
+      transition('visible => hidden', animate('0.7s cubic-bezier(0.4, 0, 0.6, 1)'))  
     ])
   ]
+  
+  
 })
 export class LoginComponent {
   @Output() closeFlyer = new EventEmitter<void>();
