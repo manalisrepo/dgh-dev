c91655f1e7e1358ae9e1477d802772e4b012eebe
manalisrepo <manalid.job@gmail.com>
2025-03-12 11:10:29 +0530

    Side Nav added

--- FE/ui-project/src/app/app.module.ts
+++ FE/ui-project/src/app/app.module.ts
@@ -6,10 +6,30 @@ import {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './pages/sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, SidenavComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule,
+    RouterModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
